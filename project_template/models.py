53eea85968e706f1ec2be43d3233f624333995e5
DpWhitt6 <zy1pk0z7@students.codeinstitute.net>
2025-05-16 21:11:31 +0100

    Create project information app pt1

--- project_template/models.py
+++ project_template/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.forms import ModelForm
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+class Submission(models.Model):
+    Tender = models.Sum('prelims'+'materials'+'labour'+'overheads'+'subcontractors')
+    widgets = {
+        'prelims': forms.NumberInput(attrs={'minlength': 1, 'maxlength': 15, 'required': True, 'type': 'number',}),
+        'material': forms.NumberInput(attrs={'minlength': 1, 'maxlength': 15, 'required': True, 'type': 'number',}), 
+        'labour': forms.NumberInput(attrs={'minlength': 1, 'maxlength': 15, 'required': True, 'type': 'number',}),
+        'overheads': forms.NumberInput(attrs={'minlength': 1, 'maxlength': 15, 'required': True, 'type': 'number',}),
+        'subcontractors': forms.NumberInput(attrs={'minlength': 1, 'maxlength': 15, 'required': True, 'type': 'number',}),
+    }
--- project_template/views.py
+++ project_template/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the index view.")
+
+def form_view(request):
+    if request.method == 'POST':
+        # Handle form submission
+        pass
+    else:
+        # Render the form
+        pass
+    return render(request, 'form_template.html')
